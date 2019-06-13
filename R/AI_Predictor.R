d86d49c3ec8454c16366842bcbd47ac6359e6bfa
Yiqiao Yin <32333117+yiqiao-yin@users.noreply.github.com>
2019-06-13 16:22:52 -0400

    Add files via upload

--- R/AI_Predictor.R
+++ R/AI_Predictor.R
@@ -29,8 +29,7 @@ AI_Predictor <- function(
       cl_price / lag(lag(lag(lag( lag(lag(lag(lag( lag(lag(cl_price)) )))) )))) - 1,
       cl_price / lag(lag(lag(lag(lag(lag(lag(lag(lag(lag(lag(cl_price))))))))))) - 1
   )))
-  data <- data.frame(na.omit(data))
-  all <- data
+  data <- data.frame(na.omit(data)); all <- data
   GBM_Result <- YinsLibrary::Gradient_Boosting_Machine_Classifier(
     x = data.frame(
       cbind(
@@ -128,34 +127,67 @@ AI_Predictor <- function(
   iRF_Result$Prediction.Table
   iRF_Result$AUC
   iRF_Result$Truth.vs.Predicted.Probabilities[nrow(iRF_Result$Truth.vs.Predicted.Probabilities), 2]
+  BART_Result <- YinsLibrary::Bayesian_Additive_Regression_Tree_Classifier(
+    x = data.frame(
+      cbind(
+        all[,-1][, sample(1:(ncol(all) - 1), (ncol(all) - 1))],
+        all[,-1][, sample(1:(ncol(all) - 1), (ncol(all) - 1))],
+        all[,-1][, sample(1:(ncol(all) - 1), (ncol(all) - 1))],
+        all[,-1][, sample(1:(ncol(all) - 1), (ncol(all) - 1))],
+        all[,-1][, sample(1:(ncol(all) - 1), (ncol(all) - 1))],
+        all[,-1][, sample(1:(ncol(all) - 1), (ncol(all) - 1))],
+        all[,-1][, sample(1:(ncol(all) - 1), (ncol(all) - 1))],
+        all[,-1][, sample(1:(ncol(all) - 1), (ncol(all) - 1))],
+        all[,-1][, sample(1:(ncol(all) - 1), (ncol(all) - 1))],
+        all[,-1][, sample(1:(ncol(all) - 1), (ncol(all) - 1))]
+      )
+    ),
+    y = all[, 1],
+    cutoff = 0.5,
+    num.tree = 10,
+    num.cut = 100,
+    cutoff.coefficient = 1,
+    SV.cutoff = 1:4
+  )
+  BART_Result$Prediction.Table; BART_Result$AUC
 
   # Return
   return(list(
     GBM_Result = list(
       Test.Confusion.Matrix = GBM_Result$Test.Confusion.Matrix,
-      Test.AUC = GBM_Result$Test.AUC),
+      Test.AUC = GBM_Result$Test.AUC,
+      Test.Errors = GBM_Result$Test.Y.Errors),
     Bagging_Result = list(
       Prediction.Table = Bagging_Result$Prediction.Table,
-      Testing.Accuracy = Bagging_Result$Testing.Accuracy),
+      Testing.Accuracy = Bagging_Result$Testing.Accuracy,
+      Test.Errors = plyr::count(Bagging_Result$test.y.hat - Bagging_Result$test.y.truth)),
     RF_Result = list(
       Prediction.Table = RF_Result$Prediction.Table,
-      AUC = RF_Result$AUC),
+      AUC = RF_Result$AUC,
+      Test.Errors = RF_Result$test.y.errors),
     iRF_Result = list(
       Prediction.Table = iRF_Result$Prediction.Table,
-      AUC = iRF_Result$AUC),
+      AUC = iRF_Result$AUC,
+      Test.Errors = plyr::count(as.numeric(iRF_Result$test.y.hat - 1L) - iRF_Result$test.y.truth)),
+    BART_Result = list(
+      Prediction.Table = BART_Result$Prediction.Table,
+      AUC = BART_Result$AUC,
+      Test.Errors = plyr::count(BART_Result$Test.Y.Hat - as.numeric(BART_Result$Test.Y.Truth))),
     Candidates_Recommendation = data.frame(
-      Name = c("GBM", "BAG", "RF", "iRF"),
+      Name = c("GBM", "BAG", "RF", "iRF", "BART"),
       Probability = c(
-        GBM_Result$Test.Y.Hat[length(GBM_Result$Test.Y.Hat)],
+        GBM_Result$Test.Y.Hat.Original[length(GBM_Result$Test.Y.Hat.Original)],
         Bagging_Result$test.y.hat[length(Bagging_Result$test.y.hat)],
         as.numeric(RF_Result$test.y.hat[length(RF_Result$test.y.hat)]),
-        iRF_Result$Truth.vs.Predicted.Probabilities[nrow(iRF_Result$Truth.vs.Predicted.Probabilities), 2])),
+        iRF_Result$Truth.vs.Predicted.Probabilities[nrow(iRF_Result$Truth.vs.Predicted.Probabilities), 2],
+        BART_Result$Test.Y.Hat[length(BART_Result$Test.Y.Hat)])),
     Conclusion = paste0(
       "Entered ticker goes up today with probability: ",
       round(mean(c(
         GBM_Result$Test.Y.Hat[length(GBM_Result$Test.Y.Hat)],
         Bagging_Result$test.y.hat[length(Bagging_Result$test.y.hat)],
         RF_Result$test.y.hat[length(RF_Result$test.y.hat)],
-        iRF_Result$Truth.vs.Predicted.Probabilities[nrow(iRF_Result$Truth.vs.Predicted.Probabilities), 2])), 3))
+        iRF_Result$Truth.vs.Predicted.Probabilities[nrow(iRF_Result$Truth.vs.Predicted.Probabilities), 2],
+        BART_Result$Test.Y.Hat[length(BART_Result$Test.Y.Hat)])), 4))
   ))
 } # End of function
