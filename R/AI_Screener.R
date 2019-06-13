46ff26d3c21d195789ce3f7da6bc1da61b34fb87
Yiqiao Yin <32333117+yiqiao-yin@users.noreply.github.com>
2019-06-13 00:13:14 -0400

    Add files via upload

---
+++ R/AI_Screener.R
@@ -0,0 +1,24 @@
+#' @title AI Screener for Entered Tickers
+#' @description AI Screener Desgiend to Filter Stocks
+#' @param
+#' @return
+#' @examples AI_Screener()
+#' @export AI_Screener
+#'
+#' # Define function
+AI_Screener <- function(
+  tickers = c("AAPL", "FB"),
+  threshold = 0.03
+) {
+  screening_process <- lapply(tickers, function(x) YinsCapital::AI_Predictor(x, threshold = threshold)$Candidates_Recommendation)
+  names(screening_process) <- tickers
+  all_stocks_prob <- c()
+  for (i in 1:length(tickers)) { all_stocks_prob <- c(all_stocks_prob, mean(data.frame(screening_process[i])[,2])) }
+  final_data <- cbind(tickers = tickers, prob = all_stocks_prob)
+  final_data <- final_data[order(as.numeric(as.character(final_data[, 2])), decreasing = TRUE), ]
+  return(list(
+    Screening_Process = screening_process,
+    Final_data = final_data
+  ))
+}
+
